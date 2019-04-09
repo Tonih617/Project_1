bb3669cd6458f8314ab360064870efd162f37a9d
Toni Hammond <48496063+Tonih617@users.noreply.github.com>
2019-04-08 22:57:00 -0400

    updated again
    citations and years were added to a select few quotes.

--- js/script.js
+++ js/script.js
@@ -5,30 +5,38 @@
 
 var quotes = [
    {quote: "For some there is a method to their madness, For others there is only madness to their methods",
-    source: "-Toni Hammond",
+    source: "Toni Hammond",
     year: "2019"},
 
    {quote: "Start by doing what's necessary, then do what's possible, and suddenly you are doing the impossible", 
-   source: "-Francis of Assisi"},
+   source: "Francis of Assisi"},
 
    {quote: "Believe you can and you're halfway there", 
-   source: "-Theodore Roosevelt"},
+    citation: "www.goodreads.com",
+   source: "Theodore Roosevelt"},
 
    {quote: "It does not matter how slowly you go as long as you do not stop",
-   source: "-Confucius"},
+    citation: "www.brainyquote.com",
+    source: "Confucius"},
 
    {quote: "Our greatest weakness lies in giving up. The most certain way to succeed is always to try just one more time",
-   source: "-Thomas A. Edison"},
+    source: "Thomas A. Edison"},
 
    {quote: "Don't watch the clock; do what it does. Keep going", 
-   source: "-Sam Levenson"},
+    source: "Sam Levenson"},
 
    {quote: "The will to win, the desire to succeed, the urge to reach your full potential... these are the keys that will unlock the door to personal excellence", 
-   source: "-Confucius"
+    source: "Confucius"},
+
+   {quote: "Grouches are nearly always pinheads, small men who have never made any effort to improve their mental capacity", 
+    source: "Thomas Edison",
+    citation: "todayinsci.com",
+    year: "Jan 1921"
+
   }];
  
 var source =[
-  {source: "ToniHammond, FrancisOfAssisi, TheodoreRoosevelt, Confucius, ThomasAEdison, Confucius, SamLevenson"
+  { source: "ToniHammond, FrancisOfAssisi, TheodoreRoosevelt, Confucius, ThomasAEdison, Confucius, SamLevenson"
   }];
 
 console.log 
@@ -73,3 +81,4 @@ quotebox.innerHTML= elements;
 console.log
 
 document.getElementById('loadQuote').addEventListener("click", printQuote, false);
+
